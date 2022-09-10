149139324bed97713dec1c4f9b64c6c29cdb36f4
Lilystef <ajuluchukwuese206@gmail.com>
2022-09-09 17:00:03 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,30 +6,23 @@
  * main - entry point
  *
  * Return:always return 0
- *
  */
 int main(void)
 {
-  /**
-   * variable n has neen declared
-   * coding then assigns the random value.
-   */
-   int n;
-
-   srand(time(0));
-   n = rand() - RAND_MAX / 2;
-  
-   if (n < 0)
-   {		   
-     printf("%d is negative\n", n);
-   }
-   else if (n == 0)
-   { 
-      printf("%d is zero\n", n);
-   }
-   else
-   {
-      printf("%d is positive\n", n);
-   }
-    return (0);
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is positive\n", n);
+	}
+	return (0);
 }
