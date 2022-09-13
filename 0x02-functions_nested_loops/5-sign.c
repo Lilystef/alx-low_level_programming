2479d3b1232d397696ba9e6a2c234531ac06b54e
Lilystef <ajuluchukwuese206@gmail.com>
2022-09-13 09:45:04 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,14 +10,14 @@ int print_sign(int n)
 	{
 	_putchar('+');
 	return (1);
-}
-        else if (n < 0)
-        {
-	_putchar('-');
+	}
+	else if (n < 0)
+	{
+	putchar('-');
 	return (-1);
 	}
-        else
-        {
+	else
+	{
 	_putchar('0');
 	return (0);
 	}
